7900a6714389a72cecefc1e2c9b40b04cf99a36e
MikroTik2 <111369356+MikroTik2@users.noreply.github.com>
2024-07-27 13:45:04 +0300

    initial commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,13 @@
 import { initializeApp, type FirebaseOptions } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig: FirebaseOptions = {
-     apiKey: 'AIzaSyDdwKwneszlfG2tfU0y0LxkidDTODDpCzQ',
-     authDomain: 'vue-auth-4cde3.firebaseapp.com',
-     projectId: 'vue-auth-4cde3',
-     storageBucket: 'vue-auth-4cde3.appspot.com',
-     messagingSenderId: '190247007217',
-     appId: '1:190247007217:web:f8355b2c54cb7b5c394a50',
+     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+     appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
